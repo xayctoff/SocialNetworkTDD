257fc47536ef4a49b77fcdd5ec02084089b1a977
xayctoff <petro310897@gmail.com>
2019-03-26 00:12:27 +0700

    Sign up test (this test should be here)

--- src/test/LoginTest.java
+++ src/test/LoginTest.java
@@ -1,8 +1,12 @@
 package test;
 
+import model.Database;
 import model.Login;
+import model.User;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.sql.SQLException;
 
 public class LoginTest {
 
@@ -10,6 +14,18 @@ public class LoginTest {
     public void loginClassCreationTest() {
         Login login = new Login();
         Assert.assertNotNull(login);
+    }
+
+    @Test
+    public void signUpTest() throws SQLException {
+        String login = "xayctoff";
+        String password = "111";
+        User user = new User();
+        user.setLogin(login);
+        user.setPassword(password);
+
+        int result = Database.getInstance().insert("INSERT INTO users values (1, @login, @password");
+        Assert.assertEquals(1, result);
     }
 
 }
