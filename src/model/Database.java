7be884fd3c9e76bda54985932afd90869ae53f44
xayctoff <petro310897@gmail.com>
2019-03-25 23:57:03 +0700

    Added method insert in class Database

--- src/model/Database.java
+++ src/model/Database.java
@@ -1,12 +1,33 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Database {
 
-    private Database() {
+    private static Database instance;
+    private Statement statement;
+    private Connection connection;
 
+    private Database() throws SQLException {
+        String url = "jdbc:postgresql://localhost:5432/network";
+        String username = "postgres";
+        String password = "111";
+        this.connection = DriverManager.getConnection(url, username, password);
     }
 
-    public static Database getInstance() {
-        
+    public static Database getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new Database();
+        }
+
+        return instance;
+    }
+
+    public int insert(String query) throws SQLException {
+        statement = instance.connection.createStatement();
+        return statement.executeUpdate(query);
     }
 }
