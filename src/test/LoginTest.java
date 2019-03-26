4fb7c3d088f6c3d5383dd8de04c0393daaab8f04
xayctoff <petro310897@gmail.com>
2019-03-26 14:09:36 +0700

    Sign in test

--- src/test/LoginTest.java
+++ src/test/LoginTest.java
@@ -1,9 +1,6 @@
 package test;
 
-import javafx.scene.chart.PieChart;
-import model.Database;
 import model.Login;
-import model.User;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -18,24 +15,29 @@ public class LoginTest {
     }
 
     @Test
-    public void signUpTest() throws SQLException {
+    public void signUpSuccessTest() throws SQLException {
         String login = "xayctoff";
         String password = "111";
-        User user = new User();
-        user.setLogin(login);
-        user.setPassword(password);
-
-        int result = Database.getInstance().insert("INSERT INTO users values (1, '" + login + "', '" +
-                password + "')");
+        Login newUser = new Login();
+        int result = newUser.signUp(login, password);
         Assert.assertEquals(1, result);
     }
 
     @Test
-    public void signInTest() throws SQLException {
+    public void signUpFailureTest() throws SQLException {
         String login = "xayctoff";
         String password = "111";
-        int result = Database.getInstance().select("SELECT * FROM users WHERE login = '" + login + "' AND "
-                + "password = '" + password + "'");
+        Login newUser = new Login();
+        int result = newUser.signUp(login, password);
+        Assert.assertEquals(0, result);
+    }
+
+    @Test
+    public void signInTest() {
+        String login = "xayctoff";
+        String password = "111";
+        Login authorization = new Login();
+        int result = authorization.signIn(login, password);
         Assert.assertEquals(1, result);
     }
 
