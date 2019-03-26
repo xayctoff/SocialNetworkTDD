c0529748b0690b860e7734e584aa3c3d2ee6de5c
xayctoff <petro310897@gmail.com>
2019-03-26 14:34:17 +0700

    Added method signUp in class Login

--- src/model/Login.java
+++ src/model/Login.java
@@ -1,8 +1,24 @@
 package model;
 
+import java.sql.SQLException;
+
 public class Login {
 
-    public Login() {
+    public Login() {}
 
+    public boolean signUp(String login, String password) throws SQLException {
+        Database database = Database.getInstance();
+
+        if (database.checkOnExistUser(login)) {
+            return false;
+        }
+
+        else {
+            database.insert("INSERT INTO users values('" + login + "', '" + password + "')");
+            User user = new User();
+            user.setLogin(login);
+            user.setPassword(password);
+            return true;
+        }
     }
 }
