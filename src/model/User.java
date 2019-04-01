9d2ccd9cc8a700128855eebcb4949dd7d60954b0
xayctoff <petro310897@gmail.com>
2019-04-01 14:28:33 +0700

    Added method getLogin in class User

--- src/model/User.java
+++ src/model/User.java
@@ -2,8 +2,21 @@ package model;
 
 public class User {
 
-    public User() {
-        
+    private String login;
+    private String password;
+
+    public User() {}
+
+    public void setLogin(String login) {
+        this.login = login;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public String getLogin() {
+        return login;
+    }
 }
