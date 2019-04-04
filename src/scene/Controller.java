c8471cf016048c240b4ea3d1adb20d99bef47757
xayctoff <petro310897@gmail.com>
2019-04-04 22:01:54 +0700

    Authorization form buttons & textfields added

--- src/scene/Controller.java
+++ src/scene/Controller.java
@@ -1,4 +1,22 @@
 package scene;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private Button signInButton;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private PasswordField passwordField;
+
 }
