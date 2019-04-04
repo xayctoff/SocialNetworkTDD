fcacc894001a17e3d822931aa689f9e50f661064
xayctoff <petro310897@gmail.com>
2019-04-05 01:05:57 +0700

    Main windows form buttons, label, lists & textfields added

--- src/scene/MainPageController.java
+++ src/scene/MainPageController.java
@@ -1,4 +1,55 @@
 package scene;
 
-public class MainWindowController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import model.Login;
+import model.User;
+
+public class MainPageController {
+
+    private User user;
+
+    @FXML
+    private Button acceptButton;
+
+    @FXML
+    private Button declineButton;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private Button signOutButton;
+
+    @FXML
+    private Button sendButton;
+
+    @FXML
+    private ListView friendsList;
+
+    @FXML
+    private ListView subscribersList;
+
+    @FXML
+    private ListView searchResult;
+
+    @FXML
+    private ListView dialog;
+
+    @FXML
+    private TextField searchField;
+
+    @FXML
+    private TextArea messageField;
+
+    @FXML
+    private Label loginLabel;
+
 }
