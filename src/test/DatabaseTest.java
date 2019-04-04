0300e7119e4225cb0b8556365abc68c0b6580c7d
xayctoff <petro310897@gmail.com>
2019-04-04 16:46:58 +0700

    Update test

--- src/test/DatabaseTest.java
+++ src/test/DatabaseTest.java
@@ -1,7 +1,6 @@
 package test;
 
 import model.Database;
-import model.User;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -52,6 +51,14 @@ public class DatabaseTest {
         Assert.assertFalse(result);
     }
 
-
+    @Test
+    public void updateTest() throws SQLException {
+        Database database = Database.getInstance();
+        String query = "UPDATE friends SET status = 2 WHERE first = (SELECT user_id FROM users WHERE login = " +
+                "'xayctoff' AND second = (SELECT user_id FROM users WHERE login = 'andre') OR first = (SELECT user_id" +
+                " FROM users WHERE login = 'andre') AND second = (SELECT user_id FROM users WHERE login = 'xayctoff')";
+        int result = database.update(query);
+        Assert.assertEquals(1, result);
+    }
 
 }
