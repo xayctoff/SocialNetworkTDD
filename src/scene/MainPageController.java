ba36cb0992005831216a4363bc55c7e7a9b7de97
xayctoff <petro310897@gmail.com>
2019-04-05 10:14:39 +0700

    Initialize methods added

--- src/scene/MainPageController.java
+++ src/scene/MainPageController.java
@@ -2,14 +2,14 @@ package scene;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.stage.Stage;
-import model.Login;
 import model.User;
 
-public class MainPageController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainPageController implements Initializable {
 
     private User user;
 
@@ -52,4 +52,19 @@ public class MainPageController {
     @FXML
     private Label loginLabel;
 
+    private void setLoginLabel() {
+        loginLabel.setText(user.getLogin());
+    }
+
+    private void getUser() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+        LoginController controller = loader.getController();
+        this.user = controller.getUser();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        getUser();
+        setLoginLabel();
+    }
 }
